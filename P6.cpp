cd34380d2be81d51796f2dc2f6d1330c13c9b6c2
志恒 何 <hz.h0827@outlook.com>
2020-02-13 18:36:50 +0800

    2020/2/12

--- P6.cpp
+++ P6.cpp
@@ -27,3 +27,78 @@ int P6_1()
     cout << " characters totla in sentence\n";
     return 0;
 }
+
+
+
+//例题6-2；
+int P6_2()
+{
+    char ch;
+    std::cout << "Type, and I shall repeat.\n";
+    std::cin.get(ch);
+    while (ch != '.')
+    {
+        if (ch == '\n')
+            std::cout << ch;
+        else
+            std::cout << ++ch;
+    }
+    std::cout << "\nPlease excuse the slight confusion.\n";
+    return 0;
+}
+
+
+
+//例题6-3；
+const int Fave = 27;
+int P6_3()
+{
+    int n;
+    cout << "Enter a number in the range 1-100 to find ";
+    cout << "my favorite number: ";
+    do
+    {
+        cin >> n;
+        if (n < Fave)//if else 结构的用法；
+            cout << "Too low -- guess again: ";
+        else if (n > Fave)
+            cout << "Too high -- guess again: ";
+        else
+            cout << Fave << " is right!\n";
+    } while (n != Fave);
+    return 0;
+}
+
+
+
+//例题6-4；
+int P6_4()
+{
+    cout << "This program may reformat your hard disk\n"
+        "and destory all your data.\n"
+        "Do you wish to continue?<y/n>";
+    char ch;
+    cin >> ch;
+    if (ch == 'y' || ch == 'y')
+        cout << "You were warned!\a\a\n";
+    else if (ch == 'n' || ch == 'N')
+        cout << "A wise choice ... bye\n";
+    else
+        cout << "That wasn't a y or n!Apparently you "
+        "can't follow\ninstructions,so "
+        "I'll trash your disk anyway.\a\a\a\n";
+    return 0;
+}
+
+
+
+//例题6-5；
+const int ArSize = 6;
+int P6_5()
+{
+    float naap[ArSize];
+    cout << "Enter the NAAQs (New Age Awareness Quotients) "
+        << "of\nyour neighbors.Program terminates "
+        << "when you make\n" << ArSize << " entries "
+        << "or enter a negative value.\n";
+}
